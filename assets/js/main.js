59a30faccff6e12f0915947a18837932081642b5
Brandon <brandonmhenry@gmail.com>
2019-03-17 00:12:08 -0400

    refactor and custom handle mouseup and mousedown to avoid draggable conflict

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,27 +6,25 @@ game.hand = document.getElementById("hand");
 game.suits = ["<span class='red'>♥</span>", '♣', "<span class='red'>♦</span>", '♠'];
 game.ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
-game.pile = function(cards, coords) {
+game.pile = function(cards, coords){
   let p = {};
   p.cards = cards;
   p.location = coords;
-  p.render = function() {
+  p.render = function(){
     let c = p.cards[0];
     if (!c){
       return;
     }
     c.cards_below = p.cards.length - 1;
-    console.log(p.cards.length);
     c.div.style.top = `${p.location[0]}px`;
     c.div.style.left = `${p.location[1]}px`;
     c.render();
-    console.log("binding stff");
     $(c.div).droppable({
       greedy: true,
-      drop: function( event, ui ) {
+      drop: function(event, ui){
         let new_card = ui.draggable.data("c");
         new_card.cards_below = p.cards.length;
-        console.log("dropping ", new_card.identifier, " onto ", c.identifier);
+        console.log("dropping", new_card.identifier, "onto", c.identifier);
         p.cards.unshift(new_card);
         $(c.div).droppable('disable');
         p.render();
@@ -35,14 +33,15 @@ game.pile = function(cards, coords) {
     $(c.div).draggable({
       containment: "div#board",
       stack: "#board .card",
-      distance: 4,
-      start: function(e, ui) {
+      distance: 6,
+      start: function(e, ui){
         console.log("start drag", c.identifier);
+        $(c.div).off('mouseup');
         c.div.classList.remove("pile-0", "pile-13", "pile-26", "pile-39", "pile-52");
         p.cards.shift();
         p.render();
       },
-      stop: function(e, ui) {
+      stop: function(e, ui){
         console.log("stop drag", c.identifier);
       }
     }).css({position: "absolute", top: p.location[0], left: p.location[1]});
@@ -55,51 +54,16 @@ $(game.board).droppable({
   drop: function(event,ui){
     let new_card = ui.draggable.data("c");
     new_card.cards_below = 0;
-    console.log("dropping ", new_card.identifier, " onto board");
+    console.log("dropping", new_card.identifier, "onto board");
     let new_pile = game.pile([new_card],[new_card.div.style.top, new_card.div.style.left]);
     new_pile.render();
   }
 });
 
 game.cards = []
-game.suits.forEach(function(s, si) {
-  game.ranks.forEach(function(r, ri) {
-    let card = {};
-    card.identifier = `${ri}-${si}`;
-    card.rank = r;
-    card.suit = s;
-    card.facing_up = Math.random() < 0.5;
-    card.div = document.createElement("div");
-    $(card.div).data("c", card);
-    $(card.div).click(function(e){
-      e.preventDefault();
-      card.facing_up = !card.facing_up;
-      card.render();
-    });
-    card.render = function() {
-      if (card.cards_below < 1){
-        card.div.className = "pile-0";
-      } else if(card.cards_below < 13) {
-        card.div.className = "pile-13";
-      } else if(card.cards_below < 26) {
-        card.div.className = "pile-26";
-      } else if(card.cards_below < 39) {
-        card.div.className = "pile-39";
-      } else {
-        card.div.className = "pile-52";
-      }
-      let inner = "";
-      if (card.facing_up) {
-        card.div.classList.remove('cardback');
-        inner = `<div class='rank'>${card.rank}</div><div class='suit'>${card.suit}</div>`;
-      } else {
-        card.div.classList.add('cardback');
-        inner = `<div class='rank'>&nbsp;</div><div class='suit'>&nbsp;</div>`;
-      }
-      card.div.innerHTML = inner;
-      card.div.classList.add('card');
-    }
-
+game.suits.forEach(function(s, si){
+  game.ranks.forEach(function(r, ri){
+    card = util.make_card(s, si, r, ri);
     game.cards.unshift(card);
   });
 });
@@ -108,4 +72,3 @@ game.suits.forEach(function(s, si) {
 util.shuffle(game.cards);
 game.deck = game.pile(game.cards, [40,40]);
 game.deck.render();
-
--- assets/js/util.js
+++ assets/js/util.js
@@ -1,20 +1,89 @@
 function util () {}
 
+
 util.shuffle = function (array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
+};
+
+
+util.touchDevice = false;
+window.addEventListener('touchstart', function onFirstTouch() {
+  document.body.classList.add('touch-device');
+  util.touchDevice = true;
+  window.removeEventListener('touchstart', onFirstTouch, false);
+}, false);
+
+
+util.pile_class = function(cards_below){
+  if (card.cards_below < 1){
+    "pile-0";
+  } else if(card.cards_below < 13) {
+    "pile-13";
+  } else if(card.cards_below < 26) {
+    "pile-26";
+  } else if(card.cards_below < 39) {
+    "pile-39";
+  } else {
+    "pile-52";
+  }
+};
+
+
+util.mousedown = function(c){
+  return function(e){
+    var data = {}
+    data.startclick = Date.now();
+    console.log(data.startclick);
+    $(c.div).mouseup(util.mouseup(c, data));
+  }
+}
+
+util.mouseup = function(c, data){
+  return function(e){
+    $(c.div).off('mouseup');
+    console.log(Date.now() - data.startclick);
+    if (Date.now() - data.startclick < 200){
+      c.flipcard();
+    }
+  }
+}
+
+util.make_card = function(suit, si, rank, ri){
+  let card = {};
+  card.identifier = `${ri}-${si}`;
+  card.rank = rank;
+  card.suit = suit;
+  card.facing_up = Math.random() < 0.5;
+  card.div = document.createElement("div");
+  $(card.div).data("c", card);
+  $(card.div).mousedown(util.mousedown(card));
+  card.render = function() {
+    card.div.className = util.pile_class(card.cards_below)
+    let inner = "";
+    if (card.facing_up) {
+      card.div.classList.remove('cardback');
+      inner = `<div class='rank'>${card.rank}</div><div class='suit'>${card.suit}</div>`;
+    } else {
+      card.div.classList.add('cardback');
+      inner = `<div class='rank'>&nbsp;</div><div class='suit'>&nbsp;</div>`;
+    }
+    if (util.touchDevice){
+
+    }
+    card.div.innerHTML = inner;
+    card.div.classList.add('card');
+  }
+  card.flipcard = function(){
+    card.facing_up = !card.facing_up;
+    card.render();
+  }
+  return card;
 };
